bdd4284af3c8085c396b08cb0ef9d902a8ffc95b
Pedro Cunial <pedrocunial@gmail.com>
2023-08-07 13:50:01 -0300

    update project structure

---
+++ target/debug/aoc01.d
@@ -0,0 +1,1 @@
+/home/gil/projects/advent_of_coders/aoc01/target/debug/aoc01: /home/gil/projects/advent_of_coders/aoc01/src/main.rs
---
+++ target/debug/deps/aoc01-cb263d18b5da8d39.d
@@ -0,0 +1,5 @@
+/home/gil/projects/advent_of_coders/aoc01/target/debug/deps/aoc01-cb263d18b5da8d39.rmeta: src/main.rs
+
+/home/gil/projects/advent_of_coders/aoc01/target/debug/deps/aoc01-cb263d18b5da8d39.d: src/main.rs
+
+src/main.rs:
